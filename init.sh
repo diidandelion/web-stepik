44918e4ccb315ba8b219eff35b1d4a82c40d915d
Ramil Bikmaev <bikmaeff@gmail.com>
2016-05-18 23:29:39 +0300

    new init

--- init.sh
+++ init.sh
@@ -1,6 +1,25 @@
-sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
-sudo rm -rf /etc/nginx/sites-enabled/default
-sudo /etc/init.d/nginx restart
+#sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
+#sudo rm -rf /etc/nginx/sites-enabled/default
+#sudo /etc/init.d/nginx restart
 #sudo rm /etc/gunicorn.d/test
-sudo ln -sf /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
+#sudo ln -sf /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
+#sudo /etc/init.d/gunicorn restart
+
+#!/bin/bash
+
+# Install
+sudo pip install django-autofixture pytz
+sudo apt-get install -y w3m
+
+# Nginx
+if [ -f /etc/nginx/sites-enabled/default ]; then
+  sudo rm /etc/nginx/sites-enabled/default
+fi
+touch /home/box/nginx.log
+sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/ask.conf
+sudo /etc/init.d/nginx restart
+
+# Gunicorn (ver. 17.5)
+touch /home/box/gunicorn.log
+sudo ln -sf /home/box/web/etc/gunicorn_ask.conf /etc/gunicorn.d/ask
 sudo /etc/init.d/gunicorn restart
